63ebf3e5f64b171787e4f668283db9ff6119ff6e
hdesk1038 <hdesk@igidr.ac.in>
2025-09-12 12:23:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "../public/index.css"; // TailwindCSS imports are inside here
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Description from "./Description";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    }, {
+        path: "/description",
+        element: <Description />,
+    }
+]);
 root.render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={appRouter} />
     </React.StrictMode>
 );
